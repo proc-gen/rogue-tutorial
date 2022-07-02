acab558b3d7ae57aa66665532a7d2a9f63ba9b6d
Micah Caunter <mcaunter12@gmail.com>
2022-07-02 19:49:35 -0400

    Completed Section 2.2 - Walking A Map

---
+++ RogueTutorial/Map/Map.cs
@@ -0,0 +1,33 @@
+﻿using SadConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueTutorial.Map
+{
+    internal class Map
+    {
+        private TileType[] _mapGrid;
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Map(int width, int height)
+        {
+            Height = height;
+            Width = width;
+            _mapGrid = new TileType[width * height];
+        }
+
+        public TileType GetMapCell(int x, int y)
+        {
+            return _mapGrid[y * Width + x];
+        }
+
+        public void SetMapCell(int x, int y, TileType tileType)
+        {
+            _mapGrid[y * Width + x] = tileType;
+        }
+    }
+}
---
+++ RogueTutorial/Map/TileType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SadConsole;
+using SadRogue.Primitives;
+
+namespace RogueTutorial.Map
+{
+    public enum TileType
+    {
+        Wall = '#',
+        Floor = '.'
+    }
+
+    public static class TileGlyphs
+    {
+        public static ColoredGlyph Wall = new ColoredGlyph(Color.White, Color.Black, (char)TileType.Wall);
+        public static ColoredGlyph Floor = new ColoredGlyph(Color.White, Color.Black, (char)TileType.Floor);
+    }
+}
