9df51f614e100b0c0bb0498068586e015e8a99e1
Micah Caunter <mcaunter12@gmail.com>
2022-07-03 19:25:19 -0400

    Completed Section 2.4 - Field of View. Had to pull in RogueSharp in order to do the FOV calculations. Was able to keep it encapsulated inside the existing Map class for now.

--- RogueTutorial/RootScreen.cs
+++ RogueTutorial/RootScreen.cs
@@ -19,34 +19,53 @@ namespace RogueTutorial
         World world;
         Query renderablesQuery;
         Query playerQuery;
+        List<Systems.System> systems;
 
         public RootScreen(int width, int height)
             : base(width, height)
         {
-            this.UseMouse = false;
+            createWorld(width, height);
+            initializeSystems();
+            
+            this.Surface.IsDirty = true;
+        }
 
+        private void createWorld(int width, int height)
+        {
             world = World.Create("My World");
             Map.Map map = MapGenerator.RoomsAndCorridorsGenerator(width, height);
 
-            world.SetData(map); 
-            
+            world.SetData(map);
+
             world.CreateEntity(new Position() { Point = map.Rooms.First().Center() }
-                                , new Renderable() {Glyph = new ColoredGlyph(Color.White, Color.Black, '@')}
-                                , new Player());
+                                , new Renderable() { Glyph = new ColoredGlyph(Color.Yellow, Color.Black, '@') }
+                                , new Player()
+                                , new Viewshed() { VisibleTiles = new List<Point>(), Range = 9 });
+        }
 
-            
-
+        private void initializeSystems()
+        {
             renderablesQuery = world.CreateQuery()
                                 .Has<Position>()
                                 .Has<Renderable>();
 
             playerQuery = world.CreateQuery().Has<Player>();
-            this.Surface.IsDirty = true;
-        }
 
+            systems = new List<Systems.System>();
+
+            systems.Add(new VisibilitySystem(world
+                                                , world.CreateQuery()
+                                                    .Has<Position>()
+                                                    .Has<Viewshed>()
+                                                , playerQuery));
+        }
 
         public override void Update(TimeSpan delta)
         {
+            foreach(Systems.System system in systems)
+            {
+                system.Run(delta);
+            }
             base.Update(delta);
         }
 
@@ -104,25 +123,47 @@ namespace RogueTutorial
             if (this.Surface.IsDirty)
             {
                 this.Surface.Clear();
-                var map = world.GetData<Map.Map>();
+                Map.Map map = world.GetData<Map.Map>();
+                Entity player = playerQuery.GetEntities()[0];
+                Viewshed playerVisibility = player.Get<Viewshed>();
+
                 for(int i = 0; i < map.Width; i++)
                 {
-                    for(int j = 0; j < map.Height; j++)
+                    for (int j = 0; j < map.Height; j++)
                     {
-                        switch (map.GetMapCell(i, j))
+                        if (playerVisibility.VisibleTiles.Any(a => a.X == i && a.Y == j)) 
+                        { 
+                            switch (map.GetMapCell(i, j))
+                            {
+                                case TileType.Floor:
+                                    TileGlyphs.FloorVisible.CopyAppearanceTo(this.Surface[i, j]);
+                                    break;
+                                case TileType.Wall:
+                                    TileGlyphs.WallVisible.CopyAppearanceTo(this.Surface[i, j]);
+                                    break;
+                            }
+                        }
+                        else if(map.IsMapCellExplored(i, j))
                         {
-                            case TileType.Floor:
-                                TileGlyphs.Floor.CopyAppearanceTo(this.Surface[i, j]);
-                                break;
-                            case TileType.Wall:
-                                TileGlyphs.Wall.CopyAppearanceTo(this.Surface[i, j]);
-                                break;
+                            switch (map.GetMapCell(i, j))
+                            {
+                                case TileType.Floor:
+                                    TileGlyphs.Floor.CopyAppearanceTo(this.Surface[i, j]);
+                                    break;
+                                case TileType.Wall:
+                                    TileGlyphs.Wall.CopyAppearanceTo(this.Surface[i, j]);
+                                    break;
+                            }
                         }
                     }
                 }
                 renderablesQuery.Foreach((ref Position position, ref Renderable renderable) =>
                 {
-                    renderable.Glyph.CopyAppearanceTo(this.Surface[position.Point]);
+                    Point point = position.Point;
+                    if (playerVisibility.VisibleTiles.Any(a => a.X == point.X && a.Y == point.Y))
+                    {
+                        renderable.Glyph.CopyAppearanceTo(this.Surface[position.Point]);
+                    }
                 });
             }
             base.Render(delta);
---
+++ RogueTutorial/Systems/VisibilitySystem.cs
@@ -0,0 +1,31 @@
+﻿using SimpleECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RogueTutorial.Map;
+using RogueTutorial.Components;
+
+namespace RogueTutorial.Systems
+{
+    internal class VisibilitySystem : System
+    {
+        private Query playerQuery;
+        public VisibilitySystem(World world, Query query, Query playerQuery) : base(world, query)
+        {
+            this.playerQuery = playerQuery;
+        }
+
+        public override void Run(TimeSpan delta)
+        {
+            Map.Map map = world.GetData<Map.Map>();
+            Entity player = playerQuery.GetEntities()[0];
+
+            query.Foreach((Entity entity, ref Viewshed visibility, ref Position position) => { 
+                visibility.VisibleTiles = map.ComputeFOV(position.Point.X, position.Point.Y, visibility.Range, true, entity.index == player.index && entity.version == player.version);
+            });
+        }
+    }
+}
