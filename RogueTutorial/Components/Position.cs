b0dd85307a3b1d29c7918e686114a5fdbd9fcf2d
Micah Caunter <mcaunter12@gmail.com>
2022-07-04 21:00:10 -0400

    Part way done with Section 2.6 - Dealing Damage. There's an issue with having a separate system for handling updating the walkable attribute of map tiles that I'm not sure if it's due to the map class or how it's stored in the ECS. If setting the value doesn't happen directly after an entity moves, it introduces a bug of enemies walking over each other.

--- RogueTutorial/Components/Position.cs
+++ RogueTutorial/Components/Position.cs
@@ -9,6 +9,22 @@ namespace RogueTutorial.Components
 {
     internal class Position
     {
-        public Point Point { get; set; }
+        private Point _point;
+        public Point Point 
+        { 
+            get 
+            { 
+                return _point; 
+            } 
+            
+            set
+            { 
+                PreviousPoint = _point;
+                Dirty = true; 
+                _point = value; 
+            } 
+        }
+        public Point PreviousPoint { get; private set; }
+        public bool Dirty { get; set; }
     }
 }
---
+++ RogueTutorial/Systems/PositionSystem.cs
@@ -0,0 +1,34 @@
+﻿using SimpleECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RogueTutorial.Map;
+using RogueTutorial.Components;
+
+namespace RogueTutorial.Systems
+{
+    internal class PositionSystem : ECSSystem
+    {
+        public PositionSystem(World world, Query query) : base(world, query)
+        {
+        }
+
+        public override void Run(TimeSpan delta)
+        {
+            Map.Map map = world.GetData<Map.Map>();
+
+            query.Foreach((Entity entity, ref Position position) =>
+            {
+                /*if (position.Dirty)
+                {
+                    map.SetCellWalkable(position.PreviousPoint.X, position.PreviousPoint.Y, true);
+                    map.SetCellWalkable(position.Point.X, position.Point.Y, false);
+                    position.Dirty = false;
+                }*/
+            });
+        }
+    }
+}
